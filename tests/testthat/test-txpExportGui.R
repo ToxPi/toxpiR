5b8aca35bd6839f7e00affd5c2541c6bc6342284
SkylarMarvel <swmarvel@ncsu.edu>
2022-07-15 16:18:30 +0700

    Added some tests for txpExportGui

--- tests/testthat/test-txpExportGui.R
+++ tests/testthat/test-txpExportGui.R
@@ -3,7 +3,67 @@
 ##----------------------------------------------------------------------------##
 
 test_that("We can export GUI-ready files", {
-  data(txp_example_input, package = "toxpiR")
-  data(txp_example_model, package = "toxpiR")
   
+  # Note1
+  # Modifying transformation functions in the model causes the "columns are duplicated"
+  # message to be repeated, I'm not sure if that is the desired response
+  
+  # Output file for txpExportGui()
+  data_exported <- tempfile()
+  # Load data and model
+  expect_warning({
+    gui <- txpImportGui(file.path("guiFiles", "gui_output_data.csv"))
+  })
+  # No warnings/errors expected for original imported file
+  expect_silent({
+    txpExportGui(
+      fileName = data_exported,
+      input = gui$input,
+      model = gui$model,
+      id.var = 'Name',
+      fills = gui$fills
+    )
+  })
+  # Slice-level transformation function
+  test_model <- gui$model
+  expect_warning({
+    # See Note1 above
+    txpTransFuncs(test_model)[[1]] <- function(x) log10(x)
+  })
+  expect_warning({
+    txpExportGui(
+      fileName = data_exported,
+      input = gui$input,
+      model = test_model,
+      id.var = 'Name',
+      fills = gui$fills
+    )
+  })
+  # Input-level transformation function that creates negative values
+  test_model <- gui$model
+  expect_warning({
+    # See Note1 above
+    txpTransFuncs(txpSlices(test_model)[[1]])[[1]] <- function(x) -x
+  })
+  expect_warning({
+    txpExportGui(
+      fileName = data_exported,
+      input = gui$input,
+      model = test_model,
+      id.var = 'Name',
+      fills = gui$fills
+    )
+  })
+  # Negative input values, expect 3 warnings for the 3 affected slices
+  test_input <- gui$input
+  test_input[, 6] <- -test_input[, 6]
+  expect_warning(expect_warning(expect_warning({
+    txpExportGui(
+      fileName = data_exported,
+      input = test_input,
+      model = gui$model,
+      id.var = 'Name',
+      fills = gui$fills
+    )
+  })))
 })
